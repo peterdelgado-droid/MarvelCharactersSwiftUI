397db2bd84a42d78551a56e52f4e13d057dbc562
Peter Delgado <peterrafaeldelgado@gmail.com>
2022-09-08 15:57:23 +0200

    Initial Commit

---
+++ MarvelCharactersSwiftUI/MarvelCharactersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelCharactersSwiftUIApp.swift
+//  MarvelCharactersSwiftUI
+//
+//  Created by Peter Delgado on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MarvelCharactersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
